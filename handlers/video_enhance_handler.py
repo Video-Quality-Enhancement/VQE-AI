91a78ca2c7e8e4ec8a92e81d1635b3c4f4092722
Ankit Das <54183715+TheGalaxyHunter@users.noreply.github.com>
2023-06-04 08:38:52 +0530

    Update video_enhance_handler.py

--- handlers/video_enhance_handler.py
+++ handlers/video_enhance_handler.py
@@ -4,24 +4,65 @@ from services import enhance_144p_video, enhance_240p_video, enhance_360p_video,
 def video_enhance_handler_720p():
     queue = "720p_queue"
     routing_key = "720p"
-    video_enhance_consumer(queue, routing_key, enhance_720p_video)
+    while True:
+        try:
+            video_enhance_consumer(queue, routing_key, enhance_720p_video)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt")
+            break
+        except Exception as e:
+            print(e)
+            continue
 
 def video_enhance_handler_480p():
     queue = "480p_queue"
     routing_key = "480p"
-    video_enhance_consumer(queue, routing_key, enhance_480p_video)
+    while True:
+        try:
+            video_enhance_consumer(queue, routing_key, enhance_480p_video)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt")
+            break
+        except Exception as e:
+            print(e)
+            continue
 
 def video_enhance_handler_360p():
     queue = "360p_queue"
     routing_key = "360p"
-    video_enhance_consumer(queue, routing_key, enhance_360p_video)
+    while True:
+        try:
+            video_enhance_consumer(queue, routing_key, enhance_360p_video)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt")
+            break
+        except Exception as e:
+            print(e)
+            continue
+    # video_enhance_consumer(queue, routing_key, enhance_360p_video)
 
 def video_enhance_handler_240p():
     queue = "360p_queue"
     routing_key = "360p"
-    video_enhance_consumer(queue, routing_key, enhance_240p_video)
+    while True:
+        try:
+            video_enhance_consumer(queue, routing_key, enhance_240p_video)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt")
+            break
+        except Exception as e:
+            print(e)
+            continue
 
 def video_enhance_handler_144p():
     queue = "360p_queue"
     routing_key = "360p"
-    video_enhance_consumer(queue, routing_key, enhance_144p_video)
+    while True:
+        try:
+            video_enhance_consumer(queue, routing_key, enhance_144p_video)
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt")
+            break
+        except Exception as e:
+            print(e)
+            continue
