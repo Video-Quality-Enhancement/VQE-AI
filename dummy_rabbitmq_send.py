9c84c678a4a858cf2a38469ebb7309821aa67005
Ankit Das <54183715+TheGalaxyHunter@users.noreply.github.com>
2023-06-08 13:05:36 +0530

    Update dummy_rabbitmq_send.py

--- dummy_rabbitmq_send.py
+++ dummy_rabbitmq_send.py
@@ -8,16 +8,16 @@ import dotenv
 
 dotenv.load_dotenv()
 
-url = "https://drive.google.com/uc?id=1yczEIQHc14jc0B412eB5JlouGLU4sobJ"
+url = "https://download.pexels.com/vimeo/822411462/pexels-tommy-t-16609645.mp4?fps=29.97&width=426"
 
-request = VideoEnhanceRequest(1001, 1001, url)
+request = VideoEnhanceRequest('user01', 'request01', url)
 
 request = request.__dict__
 
 connection = AMQPconnection()
 producerCh = connection.create_channel()
 # result = producerCh.queue_declare(queue="enhanced.video", durable=True)
-queue = "360p_queue"
+queue = "240p_queue"
 
 body = json.dumps(request)
 producerCh.basic_publish(exchange='', routing_key=queue, body=body)
