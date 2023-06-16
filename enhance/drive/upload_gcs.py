8529a27993ff45b8a0af65f10e5d3bf5884cf5a8
Ankit Das <54183715+TheGalaxyHunter@users.noreply.github.com>
2023-06-17 02:24:11 +0530

    Create upload_gcs.py

---
+++ enhance/drive/upload_gcs.py
@@ -0,0 +1,32 @@
+import dotenv
+import os
+import datetime
+from google.cloud import storage
+from google.oauth2.service_account import Credentials
+
+class GoogleCloudStorage:
+    def __init__(self) -> None:
+        credentials = Credentials.from_service_account_file("credentials.json")
+        self.storage_client = storage.Client(credentials=credentials, project=os.getenv("PROJECT_ID"))
+        self.bucket = self.storage_client.get_bucket(os.getenv("BUCKET_NAME"))
+
+    def upload(self, filepath: str, emailid: str=None):
+        myfile_blob = self.bucket.blob(filepath)
+        myfile_blob.upload_from_filename(filepath)
+
+        print(f"File {filepath} uploaded to {os.getenv('BUCKET_NAME')}")
+
+        # Granting access to the file to the emailid
+        # myfile_blob.acl.user(emailid).grant_read()
+
+        # get signed url for the emailid
+        # expiration_time = datetime.timedelta(hours=1)
+        # url = myfile_blob.generate_signed_url(expiration_time, method="GET")
+        # url = myfile_blob._get_download_url
+
+        return f"https://storage.googleapis.com/{os.getenv('BUCKET_NAME')}/enhanced/{filepath}"
+    
+    def list_files(self):
+        blobs = self.bucket.list_blobs()
+        for blob in blobs:
+            print(blob.name)
